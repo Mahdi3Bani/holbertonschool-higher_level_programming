27e05fd42f927c5f11e10a1660b0bd9042add57f
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-07 16:39:24 +0100

    pow

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -3,6 +3,10 @@
 
 def pow(a, b):
     p = 1
-    for i in range(1, b + 1):
+    if b < 0:
+        l = b * -1
+    for i in range(1, l + 1):
         p = p * a
+    if b < 0:
+        return 1 / p
     return (p)
