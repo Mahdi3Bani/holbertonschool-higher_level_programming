b2cae16c898b1e5e4154c4d09194c6cb659a95c5
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-08 16:54:37 +0100

    arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} argument:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
