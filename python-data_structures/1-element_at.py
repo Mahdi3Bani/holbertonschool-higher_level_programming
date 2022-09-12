046690627c49a6ee0c3a452dfd95f3456d9da307
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-12 20:19:27 +0100

    index

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,5 +2,5 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return
-    print("Element at index {:d} is {:d}".format(
+    print("Element at index {} is {}".format(
         idx, my_list[idx]))
