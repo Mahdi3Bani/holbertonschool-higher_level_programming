552c71c470349e70b4fc28e542be0ab86d5b3589
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-15 00:20:25 +0100

    add

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if len(my_list) == 0:
+        return 0
     if my_list.count(my_list[0]) == len(my_list):
         return my_list[0]
     for i in my_list:
