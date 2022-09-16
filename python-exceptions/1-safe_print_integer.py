ab719386142844a60031eefff9c72791afd55761
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-16 19:58:48 +0100

    safe

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format())
+        print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
         return False
