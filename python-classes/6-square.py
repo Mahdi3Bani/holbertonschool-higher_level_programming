873acf18942d0022078a844c2d8dab13b56de98c
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-20 22:24:40 +0100

    sqaure

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -28,8 +28,10 @@ class Square:
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple)\
-                or len(value) != 2\
-                or (value[1] < 0 and value[0] < 0):
+            or not isinstance(value[0], int)\
+            or not isinstance(value[1], int)\
+            or len(value) != 2\
+            or (value[1] < 0 and value[0] < 0):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
