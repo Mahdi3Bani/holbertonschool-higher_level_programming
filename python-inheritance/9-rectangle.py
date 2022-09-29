81c517df2109dd34caf482af630528980287a8f8
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-29 15:25:31 +0100

    rec

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle (BaseGeometry):
+    """"rectangle"""
+
+    def __init__(self, width, height):
+        """"initialization"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """"print"""
+        return str({"[Rectangle] {}/{}".format(self.__width, self.__height)})
+
+    def area(self):
+        """"implementation"""
+        return self.__width * self.__height
