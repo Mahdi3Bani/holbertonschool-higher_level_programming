612ee548af37c7681327ddbf73dcfad551084c4d
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-29 15:44:16 +0100

    square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,7 +13,6 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
 
     def area(self):
         """Intialize a new instance from Square"""
