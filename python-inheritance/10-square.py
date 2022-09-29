7e910e86d6be5aa30bd277be3e54486594532e28
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-29 15:31:26 +0100

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""square class"""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """"initialization"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """"implementation"""
+        return self.__size ** 2
