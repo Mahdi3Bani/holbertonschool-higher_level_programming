89a1221aeff8c7cdfb2dfd2610417882e9f8924e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-29 15:15:42 +0100

    base

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""basegeo class"""
+
+
+class BaseGeometry:
+    """"basegeo class"""
+
+    def area(self):
+        """"raise an exception"""
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validate value"""
+
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
