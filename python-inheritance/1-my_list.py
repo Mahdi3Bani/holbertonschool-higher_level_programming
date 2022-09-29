c196ec73309f3b1d74236f51d98b620ea039726a
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-29 13:53:19 +0100

    list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Inheretence from builtin."""
+
+
 class MyList(list):
     """"subclass"""
 
     def print_sorted(self):
         a = sorted(self)
-        print (a)
+        print(a)
