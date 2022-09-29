c3aa8d5097bad98e0b289948241733b0270ed926
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-09-29 15:39:35 +0100

    square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,9 +7,18 @@ class Square(Rectangle):
     """"square"""
 
     def __init__(self, size):
+        """Intialize a new instance from Square
+        Args:
+            size (int):The size of The new Square
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """Intialize a new instance from Square"""
         return self.__size ** 2
+
+    def __str__(self):
+        """"print"""
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
