db449ea6eaaf05cf7eebe1c353d1351e9f87bd11
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-02 22:38:15 +0100

    write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,7 +4,7 @@
 
 def write_file(filename="", text=""):
     "function to write"
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, mode='w', encoding="utf-8") as f:
         p = f.write(text)
     print(p, end='')
     f.close
