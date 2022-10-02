6b8d15e2a1cd2f1916ad3633d8ba2fe685cde642
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-02 22:25:03 +0100

    file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"function to print"
+
+
 def read_file(filename=""):
     "function to print file"
     with open('filename', encoding="utf-8") as f:
         p = f.read()
-    print(p)
+    print(p, end='')
