3cb5cda6f149e479e2eb4f24ea4a5ba9d10bb412
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-02 22:43:32 +0100

    write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 "function that appand a string to a text file"
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     "function to appand"
     with open(filename, mode='a', encoding="utf-8") as f:
         p = f.write(text)
