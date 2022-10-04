3453b32de892915219fa1a14a1451c0e3dc1d6a8
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-04 14:18:35 +0100

    json string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"JSON representation of an object (string)"
+
+
+def to_json_string(my_obj):
+    "JSON representation of an object (string)"
+
+    a = json.dumps(my_obj)
+    return a
