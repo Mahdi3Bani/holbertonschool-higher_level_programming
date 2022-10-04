eb7222b2234bd9151bfdddca9d518ec5abaf65a7
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-04 14:30:38 +0100

    json

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""returns an object (Python data structure) represented by a JSON string)"""
+"""returns an object (Python data structure)
+represented by a JSON string)"""
 import json
 """json module"""
 
 
 def from_json_string(my_str):
-    """returns an object (Python data structure) represented by a JSON string"""
+    """returns an object (Python data structure)
+    represented by a JSON string"""
 
     a = json.loads(my_str)
     return a
