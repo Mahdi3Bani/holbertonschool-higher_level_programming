cc6acbcd6419bd00dac6e9d7bcb2c1e6d277773e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-04 15:16:28 +0100

    createe from json file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 def load_from_json_file(filename):
     """writes an Object to a text file"""
 
-    with open(filename, mode='w', encoding="utf-8") as f:
-        json.load(filename)
+    with open(filename, mode='r', encoding="utf-8") as f:
+        return json.load(f)
