5b5eafb09c46224829c249c1dc5c06c447aa3ddd
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-04 20:16:33 +0100

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,9 +6,12 @@ import sys
 save_to_json_file = __import__('save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-open("add_item.json", 'a')
+name = "add_item.json"
+arg = sys.argv[1:]
 try:
     a = load_from_json_file("add_item.json")
 except ValueError:
     a = []
-save_to_json_file(a + sys.argv[1:], "add_item.json")
+
+a.append(arg)
+save_to_json_file(a, name)
