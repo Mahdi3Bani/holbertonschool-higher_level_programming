60b735bef970c9b4c79467577d476b5e2f3b8a1e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-04 20:23:30 +0100

    add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,15 +2,19 @@
 """"adds all arguments to a Python list"""
 
 import sys
+from unicodedata import name
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
 
     name = "add_item.json"
     arg = sys.argv[1:]
+
     try:
-        a = load_from_json_file("add_item.json")
+        a = load_from_json_file(name)
+
     except FileNotFoundError:
         a = []
 
