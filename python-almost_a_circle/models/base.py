228457387d09fa0465f28b3b10826f8f8159909b
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-06 15:26:28 +0100

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,3 +18,23 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """"to json string"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''''save to file'''
+        obj = []
+        with open(cls.__name__ + ".json", "w") as f:
+            if list_objs is None or list_objs == []:
+                json.dumps(obj, f)
+            else:
+                for i in list_objs:
+                    obj.append(i.to_dictionary())
+                f.write(cls.to_json_string(obj))
+
