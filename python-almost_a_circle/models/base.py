f2f32633f3f406057576a9e2c69c11de760dc0c8
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-10-06 12:30:13 +0100

    base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""""class base"""
+
+
+import json
+
+
+class Base:
+    ''''class base'''
+
+    ____nb_objects = 0
+
+    def __init__(self, id=None):
+        """"class constructor"""
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
