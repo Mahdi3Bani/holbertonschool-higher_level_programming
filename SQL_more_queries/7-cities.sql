fd85ecbbab34738705c928fb517c230fa970447c
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-08 14:48:47 +0100

    foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,9 @@
 -- create a database ana a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (ID INT AUTO_INCREMENT PRIMARY KEY, state_id INT, name VARCHAR (256) NOT NULL, FOREIGN KEY(state_id ) REFERENCES states (id))
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT,
+    name VARCHAR (256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id)
+);
