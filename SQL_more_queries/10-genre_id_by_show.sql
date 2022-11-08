d9cfdc49c00d6b974217c3b10aa9b3d0f337c389
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-08 22:01:46 +0100

    i love bb

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.title = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id <> 'NULL' 
+WHERE genre_id <> 'NULL' 
 ORDER BY tv_shows.title, tv_show_genres.genre_id
