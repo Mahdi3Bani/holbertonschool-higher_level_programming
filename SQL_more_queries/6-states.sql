c65fa5ef8a51c609fe4d9d47463f93702367b445
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-08 14:42:28 +0100

    prim key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create a database nd a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
