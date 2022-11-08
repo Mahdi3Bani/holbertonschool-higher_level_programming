01f21bc78899efc4a2689900153f0bfbc83647d4
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-08 21:54:57 +0100

    i love bb

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- sts all shows contained that have at least one genre linked
+-- lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.title = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
