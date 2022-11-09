8122b2e316eddca046e8d185dd9c22608e487bac
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-09 12:08:13 +0100

    ...

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all genres
 SELECT tv_genres.name AS 'genre',
-    COUNT(tv_show_genres.genre_id) AS 'number_shows'
+    COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres
     INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
