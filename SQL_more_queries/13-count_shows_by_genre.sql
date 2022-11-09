7c30c309fa067e3685ffc96e2249edffcd0db8ae
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-09 12:01:10 +0100

    ...

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all genres
+SELECT tv_genres.name AS genre,
+    COUNT(TS.genre_id) AS number_of_shows
+FROM tv_shows
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genres_id
+GROUP BY tv_genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows;
