3af784c2a6a3acf1aad1b7b86080e404c7575159
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-09 10:43:00 +0100

    no like

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,9 @@
 -- sts all shows contained that have at least one genre linked
 -- lists all cities contained in the database
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title,
+    tv_show_genres.genre_id
 FROM tv_shows
-LEFT OUTER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE genre_id = 'NULL' 
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+    LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title,
+    tv_show_genres.genre_id;
