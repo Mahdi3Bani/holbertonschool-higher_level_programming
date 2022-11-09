5f1d8d9d9331f8d655b79e65c63acf9a198974e9
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-09 10:32:58 +0100

    everythng

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,6 @@
--- sts all shows contained that have at least one genre linked
--- lists all cities contained in the database
+-- everything
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-FULL JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id
