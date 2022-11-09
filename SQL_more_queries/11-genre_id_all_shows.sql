e805cbd070264961e075bc0f2c153f437f73e4db
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-09 10:30:30 +0100

    all

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 FULL JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE genre_id <> 'NULL' 
+ON tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id
