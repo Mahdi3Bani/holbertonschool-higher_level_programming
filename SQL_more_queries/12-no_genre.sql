3cb0bf6d74030a19d8f8497f253df130b8f7c345
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-09 10:41:59 +0100

    no like

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 -- lists all cities contained in the database
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 WHERE genre_id = 'NULL' 
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
