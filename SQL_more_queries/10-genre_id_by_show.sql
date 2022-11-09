7065c8c40b5be2979b4d81aa9228ff8f4525c6a1
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-09 10:18:01 +0100

    new db

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- lists all cities contained in the database
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_shows.title = tv_show_genres.show_id
 WHERE genre_id <> 'NULL' 
 ORDER BY tv_shows.title, tv_show_genres.genre_id
