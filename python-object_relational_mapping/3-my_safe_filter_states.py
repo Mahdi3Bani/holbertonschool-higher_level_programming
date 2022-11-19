2891a2ca9da83dea9735ad65b89f69e822fbb548
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-19 22:29:30 +0100

    safe

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+same as previous but safe from sql injection
+"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+
+    c = db.cursor()
+    c.execute(
+        "SELECT * FROM states WHERE name LIKE  BINARY '{}' ORDER BY id"
+        .format(argv[4]))
+    result = c.fetchall()
+
+    for r in result:
+        print(r)
+
+    c.close()
+    db.close()
