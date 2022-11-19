38f64b76c26612e9310c1ead9f4f19e303428705
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-19 22:40:03 +0100

    safe

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     c = db.cursor()
     c.execute(
-        "SELECT * FROM states WHERE name LIKE  BINARY '{}' ORDER BY id"
+        "SELECT * FROM states WHERE name = %s ORDER BY id"
         .format(argv[4]))
     result = c.fetchall()
 
