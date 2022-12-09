01d848161b47dab113742a9822fe5558b28c9a84
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-09 16:10:34 +0100

    args

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+if (args.length === 2) {
+    console.log('No argument');
+}
+else if (args.length === 3) {
+    console.log('Argument found')
+}
+else {
+    console.log('Arguments found')
+}
