ad52672cf2de8a7d0ffc60451b01c1e95fdcebbc
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-09 22:37:24 +0100

    second big

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,17 +5,8 @@ const args = process.argv;
 if (isNaN(args[2])) {
   console.log('0');
 } else {
-  let a = 0;
-  for (let i = 2; i < args.length; i++) {
-    if (a > parseInt(args[i])) {
-      a = args[i];
-    }
-  }
-  let b = 0;
-  for (let j = 2; j < args.length; j++) {
-    if (parseInt(args[j]) > b && parseInt(args[j]) < a) {
-      b = args[j];
-    }
-  }
-  console.log(b);
+  const array = args.map(Number);
+  array.slice(2, args.length);
+  array.sort((a, b) => a - b);
+  console.log(array[array.length - 2]);
 }
