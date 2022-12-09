4450d9ccf9c6465439c6a14ae968de7b7e293d5e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-09 16:46:01 +0100

    loops

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const tab = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const tab = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < tab.length; i++){
-    console.log(tab[i]);
+for (let i = 0; i < tab.length; i++) {
+  console.log(tab[i]);
 }
