db44402fdead0c1259e13a742016ea1d088b2fe5
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-09 16:13:44 +0100

    args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,8 +5,8 @@ if (args.length === 2) {
 	console.log('No argument');
 }
 else if (args.length === 3) {
-	console.log('Argument found')
+	console.log('Argument found');
 }
 else {
-	console.log('Arguments found')
+	console.log('Arguments found');
 }
