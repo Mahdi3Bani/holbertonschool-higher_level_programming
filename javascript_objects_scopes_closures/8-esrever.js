ef91bd35942adbb1cd4ad44f2f87f63f81dbf684
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-12 12:25:27 +0100

    bel ma9lob

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+exports.esrever = function (list)
+ {
+let a = '';
+for (let i = 0; i < list.length / 2; i++)
+{
+    a = list[i]
+    list[i] = list[list.length - i - 1]
+    list[list.length - i - 1] = a
+
+}
+return list
+}
