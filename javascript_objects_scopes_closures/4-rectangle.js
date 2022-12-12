0bb4e1188e0cca44a0d7df0350248cc440bbe197
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-12 11:50:49 +0100

    rec

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let square = '';
+    for (let j = 0; j < this.width; j++) {
+      square = square + 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(square);
+    }
+  }
+
+  rotate () {
+    const a = this.height;
+    this.height = this.width;
+    this.width = a;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
