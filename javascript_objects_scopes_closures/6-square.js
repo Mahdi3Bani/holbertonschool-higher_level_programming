b83dec127a789399d22f8c519cd1d7c7be16f6ea
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-12 12:10:00 +0100

    x or c

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+const Rectangle = require('./5-square.js');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
