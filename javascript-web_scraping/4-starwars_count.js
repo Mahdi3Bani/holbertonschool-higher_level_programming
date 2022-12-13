bdce20e4b351dc0318957c914b5c569f6e5d1dbd
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-12-13 15:29:05 +0100

    star wars

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+let movies;
+let charac;
+let i = 0;
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) throw error;
   const content = JSON.parse(body);
-  let movies;
-  let charac;
-  let i = 0;
+
   for (movies in content.results) {
     for (charac in content.results[movies].characters) {
       if (content.results[movies].characters[charac] === 'https://swapi-api.hbtn.io/api/people/18/') {
